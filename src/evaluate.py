55dae9ddadad5d13c50a43aa67bbfee565256fd1
isanoob1897@outlook.com <amnesia@localhost>
2024-03-31 00:08:49 +0000

    add

---
+++ src/evaluate.py
@@ -0,0 +1,20 @@
+# Interpreter for Camel C
+
+class Evaluate:
+    def __init__(self, ast):
+        # objects
+        self.ast = ast
+        self.functions = {}
+        
+    def evaluate(self):
+        # eval asts
+        # it should only be able to handle functions
+        for funcName, body in self.ast:
+            if funcName == 'main':
+                return body
+            else:
+                self.functions[funcName] = body
+
+inter = Evaluate([('main', 23)])
+result = inter.evaluate()
+print(result)
