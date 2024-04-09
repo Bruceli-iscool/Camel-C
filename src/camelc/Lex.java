05968e630d9db45214a79c4170fc87cc8ce9372c
Bruce Li <isanoob1897@outlook.com>
2024-04-09 23:46:55 +0000

    add and fix

--- src/camelc/Lex.java
+++ src/camelc/Lex.java
@@ -3,11 +3,40 @@ package camelc;
 import java.util.*;
 
 public class Lex {
-    public static void lex(String input) {
+    public static void main(String args[]) {
+        System.out.println(lex("(){}"));
+    }
+    public static Map<String, String> lex(String input) {
         // tokens map
         Map<String, String> Tokens = new HashMap<String, String>();
+        Map<String, String> TokensInInput = new HashMap<String, String>();
         Tokens.put("RPAREN", ")");
         Tokens.put("LPAREN", "(");
         
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            String z = "";
+            switch(c) {
+                case ')':
+                TokensInInput.put("RPAREN", ")");
+                break;
+                case '(':
+                TokensInInput.put("LPAREN", "(");
+                break;
+                case '{':
+                TokensInInput.put("LBRACE", "{");
+                break;
+                case '}':
+                TokensInInput.put("RBRACE", "}");
+                break;
+                default:
+                switch(z) {
+                    case "":
+                    z = z + c;
+                }
+            }
+        }
+        return TokensInInput;
+        // Tokens.containsKey(string)
     }
 }
