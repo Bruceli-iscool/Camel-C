7f17c9987f6ebde054f82e96675602b163d0eb95
Bruce Li <isanoob1897@outlook.com>
2024-04-09 22:53:37 +0000

    add and fix

---
+++ src/camelc/Lex.java
@@ -0,0 +1,13 @@
+package camelc;
+
+import java.util.*;
+
+public class Lex {
+    public static void lex(String input) {
+        // tokens map
+        Map<String, String> Tokens = new HashMap<String, String>();
+        Tokens.put("RPAREN", ")");
+        Tokens.put("LPAREN", "(");
+        
+    }
+}
