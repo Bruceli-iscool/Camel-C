58256e0d7de02c12fcd7fb908ae7f60f14296a42
Bruce Li <isanoob1897@outlook.com>
2024-04-09 20:01:09 -0700

    add and update

--- src/camelc/Lex.java
+++ src/camelc/Lex.java
@@ -1,43 +1,20 @@
 package camelc;
 
-import java.util.*;
 import java.util.ArrayList;
-import java.util.Arrays.*;
+
 public class Lex {
-    public static void main(String args[]) {
-        System.out.println(lex("(){}"));
-    }
-    public static Map<String, String> lex(String input) {
-        // tokens map
-        Map<String, String> Tokens = new HashMap<String, String>();
-        ArrayList<String> TokensInInadd = new ArrayList<String>();
-        Tokens.put("RPAREN", ")");
-        Tokens.put("LPAREN", "(");
-        
+    public static ArrayList<String> lex(String input) {
+        // list of tokens i String
+        ArrayList<String> result = new ArrayList<String>();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            String z = "";
             switch(c) {
-                case ')':
-                TokensInInadd.add(")");
-                break;
                 case '(':
-                TokensInInadd.add("LPAREN", "(");
-                break;
-                case '{':
-                TokensInInadd.add("LBRACE", "{");
-                break;
-                case '}':
-                TokensInInadd.add("RBRACE", "}");
                 break;
                 default:
-                switch(z) {
-                    case "":
-                    z = z + c;
-                }
+                break;
             }
         }
-        return TokensInInadd;
-        // Tokens.containsKey(string)
+        return result;
     }
 }
