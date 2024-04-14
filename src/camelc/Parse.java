55777be5d8541ac0fa5381a7042c2dec9c56866b
Bruce Li <isanoob1897@outlook.com>
2024-04-14 13:49:46 -0700

    add

--- src/camelc/Parse.java
+++ src/camelc/Parse.java
@@ -8,13 +8,14 @@ public class Parse {
     public static Map<String, Integer> ints = new HashMap<String, Integer>();
     public static void main(String[] args) {
         ArrayList<String> tokens = Lex.lex("void main() { printf(\"Hello\"); printf(\"Hello\"); }");
+        System.out.println(tokens);
         parse(tokens);
     }
     public static void parse(ArrayList<String> input) {
         // parse and execute
         // recursive parsing
         String current = input.get(0);
-        while (true) {
+        //while (!input.isEmpty()) {
             if (current.matches("void")) {
                 input.remove(0);
                 current = input.get(0);
@@ -37,23 +38,23 @@ public class Parse {
                                     input.remove(0);
                                 }
                             } else {
-                                System.err.println("Camel-C: Expected '{'");
-                                break;
+                                System.out.println("Camel-C: Expected '{'");
+                                //break;
                             }
                         } else {
-                            System.err.println("Camel-C: Expected closing ')'.");
-                            break;
+                            System.out.println("Camel-C: Expected closing ')'.");
+                            //break;
                         }
                     } else {
-                        System.err.println("Camel-C: Expected '('");
-                        break;
+                        System.out.println("Camel-C: Expected '('");
+                        //break;
                     }
                 } else if (current.matches("[a-zA-Z_][a-zA-Z0-9_]*")&& !current.matches("main")) {
                     // handle function definitions
                 } else {
-                    break;
+                    //break;
                 }
-            }
+            //}
         }
     }
     private static ArrayList<String> statements(ArrayList<String> input, String x) {
@@ -61,11 +62,13 @@ public class Parse {
         // standard output
         while (true) {
             if (x.matches("printf")) {
+                System.err.print(x);
                 input.remove(0);
                 x = input.get(0);
                 if (x.matches("\\(")) {
                     input.remove(0);
                     x = input.get(0);
+                    while (true) {
                     // handle strings
                     if (x.matches("\"")) {
                         input.remove(0);
@@ -83,26 +86,31 @@ public class Parse {
                                     System.out.print(out);
                                     input.remove(0);
                                     x = input.get(0);
+                                    System.out.println(x);
                                     continue;
                                 } else {
-                                    System.err.println("Camel-C: Expected ';'");
-                                    break;
+                                    System.out.println("Camel-C: Expected ';'");
+                                    //break;
                                 }
                             } else {
-                                System.err.println("Camel-C: Expected ')'");
-                                break;
+                                System.out.println("Camel-C: Expected ')'");
+                                //break;
                             }
                         } else {
-                            System.err.println("Camel-C: Expected '\"'");
-                            break;
+                            System.out.println("Camel-C: Expected '\"'");
+                            //break;
                         }
                     }
+                    }
                 } else {
-                    System.err.println("Camel-C: Expected '('");
-                    break;
+                    System.out.println("Camel-C: Expected '('");
+                    //break;
                 }
-            } else if (x.matches("int")) {
+            } else {
+                break;
+            }/* else if (x.matches("int")) {
                 // handle variables
+                System.out.print("x");
                 input.remove(0);
                 x = input.get(0);
                 if (x.matches("[a-zA-Z_][a-zA-Z0-9_]*")) {
@@ -112,7 +120,7 @@ public class Parse {
                     if (x.matches("=")) {
                         input.remove(0);
                         x = input.get(0);
-                        if (x.matches("[0-9_]*")) {
+                        if (x.matches("^-?\\d+$")) {
                             int y = Integer.parseInt(x);
                             ints.put(name,  y);
                             input.remove(0);
@@ -122,16 +130,23 @@ public class Parse {
                                 x = input.get(0);
                                 continue;
                             } else {
-                                System.err.println("Camel-C: Expected ';'");
+                                System.out.println("Camel-C: Expected ';'");
                                 break;
                             }
+                        } else {
+                            System.out.println("Camel-C: Expected an Integer");
+                            break;
                         }
+                    } else {
+                        System.out.println("Camel-C: Expected '='");
+                        break;
                     }
-                }
-            } else {
-                break;
-            } 
-        }
+                } else {
+                    System.out.println("Camel-C: Expected Identifier");
+                    break;
+                } */
+        }  
+        
         return input;
     }
 }
