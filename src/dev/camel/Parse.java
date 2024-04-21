112968f0ad13ea4ea7a03909f0fa9a1ef277d480
Bruce Li <isanoob1897@outlook.com>
2024-04-20 18:21:37 -0700

    add

--- src/dev/camel/Parse.java
+++ src/dev/camel/Parse.java
@@ -51,7 +51,6 @@ public class Parse {
                 } else if (current.matches("[a-zA-Z_][a-zA-Z0-9_]*")&& !current.matches("main")) {
                     // handle function definitions
                     String name = current;
-                    String funcName = current;
                     if (current.matches("\\(")) {
                         input.remove(0); 
                         current = input.get(0);
