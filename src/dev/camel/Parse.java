0cdbc059fe513b36f4b2f50d5c7a7f9ea18f53ac
Bruce Li <isanoob1897@outlook.com>
2024-04-24 20:12:08 +0000

    add

--- src/dev/camel/Parse.java
+++ src/dev/camel/Parse.java
@@ -11,14 +11,14 @@ public class Parse {
     public static Map<String, String> strings = new HashMap<String, String>();
     public static HashMap<String, HashMap<ArrayList<String>, ArrayList<String>>> voidFunc = new HashMap<String, HashMap<ArrayList<String>, ArrayList<String>>>();
     public static void main(String[] args){
-        ArrayList<String> tokens = Lex.lex("void main() { printf(\"Hello\n\"); printf(5); int hi = 5+5*23; printf(hi);}");
+        ArrayList<String> tokens = Lex.lex("void john(args) {printf(args)} void main() { printf(\"Hello\n\"); printf(5); int hi = 5+5*23; printf(hi);}");
         parse(tokens);
     }
     public static void parse(ArrayList<String> input){
         // parse and execute
         // recursive parsing
+        String current = input.get(0);
         while (!input.isEmpty()) {
-                String current = input.get(0);
                 if (current.matches("void")) {
                     input.remove(0);
                     current = input.get(0);
@@ -38,8 +38,15 @@ public class Parse {
                                     input = statements(input, current);
                                     current = input.get(0);
                                     if (current.matches("}")) {
-                                        input.remove(0);
+                                        try {
+                                            input.remove(0);
+                                            current = input.get(0);
+                                        } catch (IndexOutOfBoundsException) {
+                                            
+                                        }
                                         continue;
+                                    } else {
+                                        System.out.println("Camel-C: Expected '}'");
                                     }
                                 } else {
                                     System.err.println("\nCamel-C: Expected '{'");
@@ -55,13 +62,14 @@ public class Parse {
                         }
                     } else if (current.matches("[a-zA-Z_][a-zA-Z0-9_]*")&& !current.matches("main")) {
                         // handle function definitions
-
                         String name = current;
+                        input.remove(0);
+                        current = input.get(0);
                         if (current.matches("\\(")) {
                             input.remove(0); 
                             current = input.get(0);
                             ArrayList<String> add_to = new ArrayList<String>();
-                            while (!current.matches(")")) {
+                            while (!current.matches("\\)")) {
                                 add_to.add(current);
                                 input.remove(0);
                                 current = input.get(0);
@@ -76,7 +84,7 @@ public class Parse {
                                     String to_add = "";
                                     ArrayList<String> body = new ArrayList<String>();
                                     while (stack > 0) {
-                                        if (current.matches("{")) {
+                                        if (current.matches("\\{")) {
                                             stack += 1;
                                             to_add += current;
                                             input.remove(0);
@@ -99,9 +107,20 @@ public class Parse {
                                         }
                                         body.add(to_add);
                                     }
+                                    input.remove(0);
+                                    current = input.get(0);
                                     continue;
+                                } else {
+                                    System.err.println("Camel-C: Expected '{'");
+                                    break;
                                 }
+                            } else {
+                                System.err.println("Camel-C: Expected ')'");
+                                break;
                             }
+                        } else {
+                            System.err.println("Camel-C: Expected '('");
+                            break;
                         }
                     } else {
                     System.err.println("Camel-C: Unknown token " + input.get(0));
